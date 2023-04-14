d24d5c4cce163a0cf65659504d2e71856ce3c182
Lukas Lösche <lukas@zockbar.de>
2023-04-14 18:55:59 +0200

    Bump 0.8.1 (#44)

--- cloud2sql/__init__.py
+++ cloud2sql/__init__.py
@@ -2,7 +2,7 @@
 Cloud2Sql
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
 Read infrastructure data from your cloud and export it to a SQL database.
-:copyright: © 2022 Some Engineering Inc.
+:copyright: © 2023 Some Engineering Inc.
 :license: Apache 2.0, see LICENSE for more details.
 """
 
@@ -10,5 +10,5 @@ __title__ = "cloud2sql"
 __description__ = "Read infrastructure data from your cloud and export it to a SQL database."
 __author__ = "Some Engineering Inc."
 __license__ = "Apache 2.0"
-__copyright__ = "Copyright © 2022 Some Engineering Inc."
-__version__ = "0.8.0"
+__copyright__ = "Copyright © 2023 Some Engineering Inc."
+__version__ = "0.8.1"
